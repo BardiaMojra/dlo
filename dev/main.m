b7b070ea19fabb6e678c327d8892756638027f02
bardi <bardia.mojra@gmail.com>
2022-09-08 16:27:56 -0500

    Updated with git-express on 2022-09-08.

--- dev/main.m
+++ dev/main.m
@@ -20,18 +20,18 @@ piExct_mdl  = pi.est(pi.X, pi.Y, "piDMD exact", "exact"); % piDMD baseline
 
 % piDMD methods B000
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD ExactSVDS", "exactSVDS"); % 01
-C_mdl  = pi.est(pi.X, pi.Y, "piDMD Orth", "orthogonal"); % 02
+%C_mdl  = pi.est(pi.X, pi.Y, "piDMD Orth", "orthogonal"); % 02
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD UpTri", "uppertriangular"); % 03
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD LoTri", "lowertriangular"); % 04
-D_mdl  = pi.est(pi.X, pi.Y, "piDMD Diag", "diagonal"); % 05
-E_mdl  = pi.est(pi.X, pi.Y, "piDMD DiagPInv", "diagonalpinv"); % 06
-F_mdl  = pi.est(pi.X, pi.Y, "piDMD DiagTLS", "diagonaltls"); % 07
+%D_mdl  = pi.est(pi.X, pi.Y, "piDMD Diag", "diagonal"); % 05
+%E_mdl  = pi.est(pi.X, pi.Y, "piDMD DiagPInv", "diagonalpinv"); % 06
+%F_mdl  = pi.est(pi.X, pi.Y, "piDMD DiagTLS", "diagonaltls"); % 07
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD SymTriDiag", "symtridiagonal"); % 08
-G_mdl  = pi.est(pi.X, pi.Y, "piDMD Circ", "circulant"); % 09
-H_mdl  = pi.est(pi.X, pi.Y, "piDMD CircTLS", "circulantTLS"); % 10
-I_mdl  = pi.est(pi.X, pi.Y, "piDMD CircUnitary", "circulantunitary"); % 11
-J_mdl  = pi.est(pi.X, pi.Y, "piDMD CircSym", "circulantsymmetric"); % 12
-K_mdl  = pi.est(pi.X, pi.Y, "piDMD CircSkwSym", "circulantskewsymmetric"); % 13 
+%G_mdl  = pi.est(pi.X, pi.Y, "piDMD Circ", "circulant"); % 09
+%H_mdl  = pi.est(pi.X, pi.Y, "piDMD CircTLS", "circulantTLS"); % 10
+%I_mdl  = pi.est(pi.X, pi.Y, "piDMD CircUnitary", "circulantunitary"); % 11
+%J_mdl  = pi.est(pi.X, pi.Y, "piDMD CircSym", "circulantsymmetric"); % 12
+%K_mdl  = pi.est(pi.X, pi.Y, "piDMD CircSkwSym", "circulantskewsymmetric"); % 13 
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD BCCB", "BCCB"); % 14
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD BCCBtls", "BCCBtls"); 
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD BCCBSkwSym", "BCCBskewsymmetric"); 
@@ -42,7 +42,7 @@ K_mdl  = pi.est(pi.X, pi.Y, "piDMD CircSkwSym", "circulantskewsymmetric"); % 13
 %C_mdl  = pi.est(pi.X, pi.Y, "piDMD SkwSym", "skewsymmetric"); 
 
 % HAVOK methods @todo investiage basis functions 
-hvk_mdl  = hvk.est(hvk.x, hvk.r, "HAVOK");
+%hvk_mdl  = hvk.est(hvk.x, hvk.r, "HAVOK");
 
 %% results
 % piDMD models
@@ -53,15 +53,15 @@ dlgr.add_mdl(piExct_mdl); % baseline
 %dlgr.add_mdl(A_mdl);
 %dlgr.add_mdl(B_mdl);
 %dlgr.add_mdl(C_mdl);
-dlgr.add_mdl(C_mdl);
-dlgr.add_mdl(D_mdl);
-dlgr.add_mdl(E_mdl);
-dlgr.add_mdl(F_mdl);
-dlgr.add_mdl(G_mdl);
-dlgr.add_mdl(H_mdl);
-dlgr.add_mdl(I_mdl);
-dlgr.add_mdl(J_mdl);
-dlgr.add_mdl(K_mdl);
+%dlgr.add_mdl(C_mdl);
+%dlgr.add_mdl(D_mdl);
+%dlgr.add_mdl(E_mdl);
+%dlgr.add_mdl(F_mdl);
+%dlgr.add_mdl(G_mdl);
+%dlgr.add_mdl(H_mdl);
+%dlgr.add_mdl(I_mdl);
+%dlgr.add_mdl(J_mdl);
+%dlgr.add_mdl(K_mdl);
 
 % HAVOK models
 %dlgr.add_mdl(hvk_mdl);
@@ -70,8 +70,8 @@ dlgr.add_mdl(K_mdl);
 dlgr.get_errs(); 
 dlgr.get_tab(); % get res table
 dlgr.sav_tab(); % sav log table - result tab
-%dlgr.plt_recons_grid();
-%dlgr.plt_models_grid();
+dlgr.plt_recons_grid();
+dlgr.plt_models_grid();
 
 %% report
 %rpt.gen_plots(cfg.dat, dlog, piDMD);
